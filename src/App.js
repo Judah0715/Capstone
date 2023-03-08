ffdec435f30ad220691e0c79af05a8bc16f918c6
Judah Agnew <judah.agnew121@gmail.com>
2023-03-08 14:27:55 -0800

    added update links and functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import { browserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+import Header from "./components/Header";
+import {objectToURLString} from "./components/Helper"
+
+import Main from "./pages/Main";
+import Filter from "./pages/Filter;"
+
+const App = () => {
+  useEffect(() => {
+    handleSuggBtn();
+  }, []);
+
+  const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [urlParams, setUrlParams] = useState({
+    language: "en-US",
+    sort_by: "popularity.desc",
+    include_adult: "false",
+    include_video: "false",
+    "vote_average.gte": 7,
+  });
+
+  const getMovies = async (pageNum = 1) => {
+    const proxy = "https://api.themoviedb.org/3/discover/movie";
+    const paramStr = objectToURLString(urlParams);
+    const apikey = process.env.REACT_APP_API_KEY;
+
+    let url = '${proxy}?api_key=${apikey}&page=${pageNum}${paramStr}';
+
+    let response = await fetch(url);
+    let { results } = await response.json();
+    console.log("Result :", results );
+    return results;
+  };
+
+  const getMovieID =(arr) => {
+    if (!arr || !arr.length) return;
+    const randomNum = Math.floor(Math.random() * arr.length);
+    const movieID = arr[randomNum].id;
+
+    return movieID;
+  };
+
+  const getMovieData = async (movieID) => {
+    const proxy = "https://api.themoviedb.org/3/movie/";
+    const apikey = process.env.REACT_APP_API_KEY;
+
+    let url = `${proxy}${movieID}?api_key=${apikey}&append_to_response=videos`;
+
+    await fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setMovie(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSuggBtn = async () => {
+    setLoading(true);
+    const randomPageNum = Math.floor(Math.random() * 10);
+    const movieArr = await getMovies(randomPageNum);
+    const movieID = getMovieID(movieArr);
+    getMovieData(movieID);
+    setLoading(false);
+  };
+
+  const updateFilter = (data) => {
+    setUrlParams({
+      ...urlParams,
+      ...data,
+    })
+
+    handleSuggBtn();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Header handleSuggBtn={handleSuggBtn} />
+        <Switch>
+          <Route path='/filter'>
+            <Filter updateFilter={updateFilter} />
+          </Route>
+          <Route path='/'>
+            <Main movie={movie} loading={loading} />
+          </Route>
+        </Switch>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
