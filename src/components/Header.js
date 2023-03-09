379b2cdd8f55f656120477202f5b4fc05c89db64
Judah Agnew <judah.agnew121@gmail.com>
2023-03-09 15:36:11 -0800

    created components folder + header file with filter

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = ({ handleSuggBtn }) => {
+    return (
+        <header className='header'>
+            <div className='logo'>
+                <Link to='/'>RandomMovie</Link>
+            </div>
+            <div className='buttons'>
+                <button className='btn btn-green' onClick={handleSuggBtn}>
+                    Suggest Me Movie
+                </button>
+                <button className='btn btn-grey'>
+                    <Link to='/filter'>Filters</Link>
+                </button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
