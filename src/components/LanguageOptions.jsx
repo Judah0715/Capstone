ec82580924c2b0e24faf8600206b3923db895dd1
Judah Agnew <judah.agnew121@gmail.com>
2023-03-10 09:56:54 -0800

    added language filters

---
+++ src/components/LanguageOptions.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default () => (
+  <>
+
+    <option value='American Sign Language'>American Sign Language</option>
+    <option value='British Sign Language'>British Sign Language</option>
+    <option value='Arabic'>Arabic</option>
+    <option value='Australian Sign Language'>Australian Sign Language</option>
+    <option value='Chinese'>Chinese</option>
+    <option value='Danish'>Danish</option>
+    <option value='English'>English</option>
+    <option value='German'>German</option>
+    <option value='German Sign Language'>German Sign Language</option>
+    <option value='Greek'>Greek</option>
+    <option value='Greek, Ancient (to 1453)'>Greek, Ancient (to 1453)</option>
+    <option value='Hawaiian'>Hawaiian</option>
+    <option value='Hebrew'>Hebrew</option>
+    <option value='Hindi'>Hindi</option>
+    <option value='Hungarian'>Hungarian</option>
+    <option value='Irish Gaelic'>Irish Gaelic</option>
+    <option value='Italian'>Italian</option>
+    <option value='Japanese'>Japanese</option>
+    <option value='Japanese Sign Language'>Japanese Sign Language</option>
+    <option value='Korean'>Korean</option>
+    <option value='Korean Sign Language'>Korean Sign Language</option>
+    <option value='Latin'>Latin</option>
+    <option value='Mandarin'>Mandarin</option>
+    <option value='Persian'>Persian</option>
+    <option value='Spanish'>Spanish</option>
+    <option value='Spanish Sign Language'>Spanish Sign Language</option>
+    <option value='Swedish'>Swedish</option>
+    <option value='Turkish'>Turkish</option>
+    <option value='Vietnamese'>Vietnamese</option>
+    <option value='French'>French</option>
+    <option value='French Sign Language'>French Sign Language</option>
+    <option value='Dutch'>Dutch</option>
+    <option value='Polish'>Polish</option>
+    <option value='Bulgarian'>Bulgarian</option>
+    <option value='Armenian'>Armenian</option>
+  </>
+);
